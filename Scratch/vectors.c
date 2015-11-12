d3782ee2dce62bb72c0201c9584c4f07184f5a58
michael <mpalmer16@gmail.com>
2015-11-11 21:58:18 -0600

    work work work

--- Scratch/vectors.c
+++ Scratch/vectors.c
@@ -4,45 +4,86 @@
 
 
 
-typedef enum { NORTH, SOUTH, EAST, WEST } Direction;
+typedef enum { NORTH, EAST, SOUTH, WEST } Direction;
+
+struct Point {
+	int x;
+	int y;
+};
 
 struct Vector {
-  int x;
-  int y;
-  Direction d;
-
-  // constructor function?
+	struct Point *p;
+	Direction d;
+	void (*f)(struct Vector *);
 };
 
 void move(struct Vector *);
-struct Vector * new(int x, int y, Direction d);
+void change_dir(struct Vector *);
+struct Vector * new(struct Point *, Direction);
+struct Point * point(int, int);
+void log_func(struct Vector *);
+void destroy(struct Vector *);
 
 
 int main()
 {
-  struct Vector v1 = { 0, 0, NORTH };
+	struct Vector *v1 = new(point(0,0), NORTH);
+	v1->f(v1);
+	assert(v1->d == NORTH);
 
-  struct Vector *v2_ptr = new(10, 10, WEST);
+	move(v1);
 
-  move(&v1);
-  move(v2_ptr);
+	change_dir(v1);
+	log_func(v1);
+	assert(v1->d == EAST);
 
-  return 0;
+
+	destroy(v1);
+
+	return 0;
 }
 
 void move(struct Vector *v)
 {
-  v->x += 1;
-  v->y += 1;
+	v->p->x += 1;
+	v->p->y += 1;
 }
 
-struct Vector * new(int x, int y, Direction d)
+void change_dir(struct Vector *v)
 {
-  struct Vector *v = malloc(sizeof(struct Vector));
+	assert(v);
+	v->d = (v->d += 1) % 4;
+}
 
-  v->x = x;
-  v->y = y;
-  v->d = d;
+struct Point * point(int x, int y)
+{
+	struct Point *p = malloc(sizeof(struct Point));
+
+	p->x = x;
+	p->y = y;
+
+	return p;
+}
+
+struct Vector * new(struct Point *p, Direction d)
+{
+	struct Vector *v = malloc(sizeof(struct Vector));
+	assert(p);
+
+	v->p = p;
+	v->d = d;
+	v->f = log_func;
 
   return  v;
+}
+
+void destroy(struct Vector *v)
+{
+	free(v->p), free(v);
+}
+
+void log_func(struct Vector *v)
+{
+	fprintf(stdout, "Location:\n\tx: %d, y: %d\nDirection:\n\t%d\n", \
+			v->p->x, v->p->y, v->d);
 }
